75f025363a77af419ce29667ba475358c4b5777c
Greg Laun <greg.laun@gmail.com>
2017-08-13 16:29:05 -0700

    Initial import.

---
+++ app/src/main/java/org/leafcutter/webviewapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package org.leafcutter.webviewapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://www.wikipedia.org");
+    }
+}
