bcf70d55531e14c359af2d9aacdbf18a1a2f9871
Greg Laun <greg.laun@gmail.com>
2018-11-20 21:40:02 -0500

    Add some failing tests.

---
+++ app/src/test/unit/kotlin/com/greglaun/lector/data/whitelist/HashSetWhitelistTest.kt
@@ -0,0 +1,32 @@
+package com.greglaun.lector.data.whitelist
+
+import junit.framework.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class HashSetWhitelistTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun contains() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun add() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun delete() {
+        assertTrue(false)
+    }
+
+    @Test
+    operator fun iterator() {
+        assertTrue(false)
+    }
+}
