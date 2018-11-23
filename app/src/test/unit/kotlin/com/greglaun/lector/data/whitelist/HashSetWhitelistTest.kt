a56a6fa5accd8f8ab69fad179568043979a5eb4d
Greg Laun <greg.laun@gmail.com>
2018-11-23 09:38:59 -0500

    Make some more unit tests pass.

--- app/src/test/unit/kotlin/com/greglaun/lector/data/whitelist/HashSetWhitelistTest.kt
+++ app/src/test/unit/kotlin/com/greglaun/lector/data/whitelist/HashSetWhitelistTest.kt
@@ -1,32 +1,49 @@
 package com.greglaun.lector.data.whitelist
 
+import kotlinx.coroutines.experimental.runBlocking
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
-import org.junit.Before
 import org.junit.Test
 
 class HashSetWhitelistTest {
 
-    @Before
-    fun setUp() {
+    val whiteList = HashSetWhitelist<String>()
+    val testString = "Potato"
+
+    @Test
+    fun notContains() {
+        runBlocking {
+            assertFalse(whiteList.contains(testString).await())
+        }
     }
 
     @Test
     fun contains() {
-        assertTrue(false)
-    }
-
-    @Test
-    fun add() {
-        assertTrue(false)
+        runBlocking {
+            whiteList.add(testString)
+            assertTrue(whiteList.contains(testString).await())
+        }
     }
 
     @Test
     fun delete() {
-        assertTrue(false)
+        runBlocking {
+            whiteList.add(testString).await()
+            whiteList.delete(testString).await()
+            assertFalse(whiteList.contains(testString).await())
+        }
     }
 
     @Test
     operator fun iterator() {
-        assertTrue(false)
+        runBlocking {
+            whiteList.add("apple").await()
+            whiteList.add("sauce").await()
+            whiteList.add("pancakes").await()
+            val it = whiteList.iterator()
+            while (it.hasNext()) {
+                assertTrue(it.next() in listOf("apple", "sauce", "pancakes"))
+            }
+        }
     }
 }
--- app/src/test/unit/kotlin/com/greglaun/lector/ui/speak/TtsActorKtTest.kt
+++ app/src/test/unit/kotlin/com/greglaun/lector/ui/speak/TtsActorKtTest.kt
@@ -1,6 +1,6 @@
 package com.greglaun.lector.ui.speak
 
-import junit.framework.Assert.assertTrue
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
