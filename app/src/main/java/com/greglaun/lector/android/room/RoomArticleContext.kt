bd943157f21206afa5955760c6b617c6d12801bd
Greg Laun <greg.laun@gmail.com>
2018-11-27 20:30:07 -0500

    Make context string unique in database.

--- app/src/main/java/com/greglaun/lector/android/room/RoomArticleContext.kt
+++ app/src/main/java/com/greglaun/lector/android/room/RoomArticleContext.kt
@@ -1,10 +1,11 @@
 package com.greglaun.lector.android.room
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.Index
 import android.arch.persistence.room.PrimaryKey
 import com.greglaun.lector.data.cache.ArticleContext
 
-@Entity
+@Entity(indices = arrayOf(Index(value = "contextString", unique = true)))
 data class RoomArticleContext(@PrimaryKey(autoGenerate = true) override var id: Long?,
                               override var contextString: String,
                               override var position: String = "",  // position uses md5 hash
