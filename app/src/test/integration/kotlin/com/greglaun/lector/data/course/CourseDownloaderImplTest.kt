394864d816ffed5e5d52d62ffde5cd8834392410
Greg Laun <greg.laun@gmail.com>
2019-01-05 23:30:22 -0500

    Add some tests.

--- app/src/test/integration/kotlin/com/greglaun/lector/data/course/CourseDownloaderImplTest.kt
+++ app/src/test/integration/kotlin/com/greglaun/lector/data/course/CourseDownloaderImplTest.kt
@@ -1,6 +1,8 @@
 package com.greglaun.lector.data.course
 
 import com.greglaun.lector.BuildConfig
+import junit.framework.Assert.assertTrue
+import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Test
 import java.io.File
 
@@ -10,9 +12,23 @@ class CourseDownloaderImplTest {
 
     @Test
     fun downloadAllCourseNames() {
+        runBlocking {
+            val result = courseDownloader.downloadAllCourseNames().await()
+            assertTrue(result!!.contains("Ice Cream"))
+            assertTrue(result!!.contains("Furry Friends"))
+        }
     }
 
     @Test
     fun fetchCourseDetails() {
+        runBlocking {
+            val detailsMap = courseDownloader.fetchCourseDetails(
+                    listOf("Ice Cream", "Furry Friends")).await()
+            assertTrue(detailsMap!!.keys.size > 0)
+            assertTrue(detailsMap!!.containsKey("Ice Cream"))
+            assertTrue(detailsMap!!.containsKey("Furry Friends"))
+            assertTrue(detailsMap!!.get("Ice Cream")!!.
+                    articleNames.contains("https://en.wikipedia.org/wiki/Cold-stimulus_headache"))
+        }
     }
 }
--- app/src/test/unit/kotlin/com/greglaun/lector/data/course/CourseUtilKtTest.kt
+++ app/src/test/unit/kotlin/com/greglaun/lector/data/course/CourseUtilKtTest.kt
@@ -17,6 +17,10 @@ class CourseUtilKtTest {
     fun toCourseDetailsMap() {
         val json = "[{\"name\":\"Ice Cream\",\"articles\":\"https://en.wikipedia.org/wiki/Ice_cream\\r\\nhttps://en.wikipedia.org/wiki/Cold-stimulus_headache\\r\\nhttps://en.wikipedia.org/wiki/Ice_cream_social\\r\\nhttps://en.wikipedia.org/wiki/Soft_serve\\r\\nhttps://en.wikipedia.org/wiki/Strawberry_ice_cream\\r\\nhttps://en.wikipedia.org/wiki/Chocolate_ice_cream\\r\\nhttps://en.wikipedia.org/wiki/Neapolitan_ice_cream\\r\\nhttps://en.wikipedia.org/wiki/Vanilla_ice_cream\"},{\"name\":\"Furry Friends\",\"articles\":\"https://en.wikipedia.org/wiki/Dog\\r\\nhttps://en.wikipedia.org/wiki/Cat\\r\\nhttps://en.wikipedia.org/wiki/Domestic_rabbit\\r\\nhttps://en.wikipedia.org/wiki/Gerbil\\r\\nhttps://en.wikipedia.org/wiki/Hedgehog\"}]"
         val detailsMap = toCourseDetailsMap(extractCourseMap(json))
-        assertTrue(false)
+        assertTrue(detailsMap.keys.size > 0)
+        assertTrue(detailsMap.containsKey("Ice Cream"))
+        assertTrue(detailsMap.containsKey("Furry Friends"))
+        assertTrue(detailsMap.get("Ice Cream")!!.
+                articleNames.contains("https://en.wikipedia.org/wiki/Cold-stimulus_headache"))
     }
 }
