e410aab45aad67dca191e85f41fcc3d6d1a21c98
Greg Laun <greg.laun@gmail.com>
2019-02-01 22:17:02 -0500

    Add failing Android tests for Room classes.

---
+++ app/src/androidTest/java/com/greglaun/lector/android/room/CourseArticleJoinDaoTest.kt
@@ -0,0 +1,64 @@
+package com.greglaun.lector.android.room
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class CourseArticleJoinDaoTest {
+    private lateinit var articleContextDao: ArticleContextDao
+    private var db: LectorDatabase? = null
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(
+                context, LectorDatabase::class.java).build()
+        articleContextDao = db!!.articleContextDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db?.close()
+    }
+
+    @Test
+    fun getArticlesWithCourseId() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun getMaxOccupiedPosition() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun getLeastOccupiedPosition() {
+        assertTrue(false)
+    }
+
+
+    @Test
+    fun insert() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun get() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun getNextInCourse() {
+        assertTrue(false)
+    }
+
+}
