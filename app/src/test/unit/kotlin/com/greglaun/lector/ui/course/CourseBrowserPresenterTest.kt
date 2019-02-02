8bd3f6d77e08aadb16dacd58403fcb163e7f727d
Greg Laun <greg.laun@gmail.com>
2019-02-01 21:33:41 -0500

    Add failing tests for CourseBrowserPresenter.

---
+++ app/src/test/unit/kotlin/com/greglaun/lector/ui/course/CourseBrowserPresenterTest.kt
@@ -0,0 +1,42 @@
+package com.greglaun.lector.ui.course
+
+import junit.framework.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class CourseBrowserPresenterTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun onAttach() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun onDetach() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun beginCourseDownload() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun onSaveDetailsPressed() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun onCourseDetailSelected() {
+        assertTrue(false)
+    }
+
+    @Test
+    fun onCoursesSaved() {
+        assertTrue(false)
+    }
+}
