685651c8658f47b5ead4b88f80ed34ec455ab04b
Greg Laun <greg.laun@gmail.com>
2019-02-16 23:06:35 -0500

    Tests pass for CourseBrowserPresenter.

--- app/src/test/unit/kotlin/com/greglaun/lector/ui/course/CourseBrowserPresenterTest.kt
+++ app/src/test/unit/kotlin/com/greglaun/lector/ui/course/CourseBrowserPresenterTest.kt
@@ -1,42 +1,43 @@
 package com.greglaun.lector.ui.course
 
-import org.junit.Assert.assertTrue
-import org.junit.Before
+import com.greglaun.lector.data.course.CourseDownloader
+import com.greglaun.lector.data.course.CourseMetadata
+import com.greglaun.lector.data.course.CourseSource
+import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Test
+import org.mockito.Mockito.*
 
 class CourseBrowserPresenterTest {
-
-    @Before
-    fun setUp() {
-    }
-
-    @Test
-    fun onAttach() {
-        assertTrue(false)
-    }
-
-    @Test
-    fun onDetach() {
-        assertTrue(false)
-    }
+    val mockView = mock(CourseBrowserContract.View::class.java)
+    val mockDownloader = mock(CourseDownloader::class.java)
+    val mockSource = mock(CourseSource::class.java)
+    val courseBrowserPresenter = CourseBrowserPresenter(mockView, mockDownloader, mockSource)
 
     @Test
     fun beginCourseDownload() {
-        assertTrue(false)
-    }
-
-    @Test
-    fun onSaveDetailsPressed() {
-        assertTrue(false)
+        runBlocking {
+            courseBrowserPresenter.beginCourseDownload()
+        }
+        verify(mockDownloader, times(1)).downloadCourseMetadata()
     }
 
     @Test
     fun onCourseDetailSelected() {
-        assertTrue(false)
+        runBlocking {
+                courseBrowserPresenter.onCourseDetailSelected(CourseMetadata("A Course"))
+            }
+            verify(mockDownloader, times(1)).
+                    fetchCourseDetails(CourseMetadata("A Course"))
     }
 
     @Test
     fun onCoursesSaved() {
-        assertTrue(false)
+        val courseDetails = listOf(CourseMetadata("One"),
+                CourseMetadata("Two"), CourseMetadata("Three"))
+        runBlocking {
+            val job = courseBrowserPresenter.onCoursesSaved(courseDetails)
+            verify(mockDownloader, times(1)).
+                    fetchCourseDetails(CourseMetadata("One"))
+        }
     }
 }
