66039838bfa1c3ed9df82bcd2e447abc3ad6b34b
Greg Laun <greg.laun@gmail.com>
2019-04-01 21:21:48 -0400

    Fix bug.

--- app/src/main/java/com/greglaun/lector/ui/main/MainPresenter.kt
+++ app/src/main/java/com/greglaun/lector/ui/main/MainPresenter.kt
@@ -237,7 +237,7 @@ class MainPresenter(val view : MainContract.View,
 
     override suspend fun onPageDownloadFinished(urlString: String) {
         GlobalScope.launch {
-            store.dispatch(MarkDownloadFinished(urlString))
+            store.dispatch(WriteAction.MarkDownloadFinished(urlString))
         }
 
     }
